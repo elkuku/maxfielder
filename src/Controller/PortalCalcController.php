a7a9761a06c4cc1b80fc8616f8f5d791aa66d68c
Nikolai Plath <der.el.kuku@gmail.com>
2022-11-21 11:55:48 -0500

    Add portal calc

---
+++ src/Controller/PortalCalcController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortalCalcController extends AbstractController
+{
+    #[Route('/portalcalc', name: 'app_portalcalc')]
+    public function index(): Response
+    {
+        return $this->render('portalcalc/index.html.twig');
+    }
+}
