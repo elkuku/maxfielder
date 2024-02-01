aacecd0393337c6065685b27829b854cedd908da
elkuku <der.el.kuku@gmail.com>
2024-02-01 09:12:46 -0500

    Change deprecated routing class

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Repository\MaxfieldRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use UnexpectedValueException;
 
 class DefaultController extends BaseController
--- src/Controller/Security/GoogleController.php
+++ src/Controller/Security/GoogleController.php
@@ -5,7 +5,7 @@ namespace App\Controller\Security;
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class GoogleController extends AbstractController
 {
